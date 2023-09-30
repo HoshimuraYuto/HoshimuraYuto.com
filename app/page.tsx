56227a095c78644b8129f7429b0840a2aa804bc9
HoshimuraYuto <sukiburo@gmail.com>
2023-09-30 20:09:02 +0900

    ✏️ Fix unescaped text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl">Hi 👋, I'm Hoshimura Yuto.</h1>
+      <h1 className="text-2xl">Hi 👋, I&apos;m Hoshimura Yuto.</h1>
     </main>
   );
 }
