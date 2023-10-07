b3d386748f9ad4308a60903b6aae63b0398925cf
HoshimuraYuto <sukiburo@gmail.com>
2023-10-07 18:56:56 +0000

    #1 add linter&formatter setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,46 @@
+/** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-  extends: "next/core-web-vitals",
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "next/core-web-vitals",
+    "prettier",
+  ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "import"],
+  root: true,
+  rules: {
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc", caseInsensitive: true },
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+  },
 };
