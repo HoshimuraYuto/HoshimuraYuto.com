8dd670911d998fd478160b65232fa477a23cec25
HoshimuraYuto <sukiburo@gmail.com>
2023-10-22 17:38:52 +0900

    #41 fix workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpx serve out",
+    command: "pnpm start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
