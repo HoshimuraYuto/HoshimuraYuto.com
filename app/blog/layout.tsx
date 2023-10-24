c398f47afd1ccb0330e62524723d152385d1d01e
HoshimuraYuto <sukiburo@gmail.com>
2023-10-24 23:21:39 +0900

    #32 update layout

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -2,11 +2,13 @@ import Aside from "./Aside";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center gap-10 p-10 lt-sm:p-6">
-      <aside className="w-[190px] border-0 border-r-1 border-neutral-1 border-solid dark:border-neutral-7">
-        <Aside />
-      </aside>
-      <main className="max-w-[600px] flex flex-col gap-12">{children}</main>
+    <div className="p-10 lt-sm:p-6">
+      <div className="mx-auto max-w-[830px] flex justify-center gap-10">
+        <aside className="w-[190px] border-0 border-r-1 border-neutral-1 border-solid dark:border-neutral-7">
+          <Aside />
+        </aside>
+        <main className="max-w-[600px] flex flex-col gap-12">{children}</main>
+      </div>
     </div>
   );
 };
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,4 @@
-const Page = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex justify-center gap-10 p-10 lt-sm:p-6">{children}</div>
-  );
+const Null = () => {
+  return <></>;
 };
-
-export default Page;
+export default Null;
