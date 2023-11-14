b6eca8088c815eb61748ed35db65bbecc47ece3c
HoshimuraYuto <sukiburo@gmail.com>
2023-11-14 14:35:29 +0000

    remove header story

--- tests/stories/layouts/Header.stories.tsx
+++ tests/stories/layouts/Header.stories.tsx
@@ -1,24 +1,25 @@
-import { Meta, StoryObj } from "@storybook/react";
+// import { Meta, StoryObj } from "@storybook/react";
 
-import Header from "../../../app/components/layouts/Header";
+// import Header from "../../../app/components/layouts/Header";
 
-const meta = {
-  title: "Layout/Header",
-  component: Header,
-  parameters: {
-    layout: "centered",
-  },
-  tags: ["autodocs"],
-  decorators: [
-    (Story) => (
-      <div style={{ minWidth: "600px" }}>
-        <Story />
-      </div>
-    ),
-  ],
-} satisfies Meta<typeof Header>;
+// const meta = {
+//   title: "Layout/Header",
+//   component: Header,
+//   parameters: {
+//     layout: "centered",
+//   },
+//   tags: ["autodocs"],
+//   decorators: [
+//     (Story) => (
+//       <div style={{ minWidth: "600px" }}>
+//         <Story />
+//       </div>
+//     ),
+//   ],
+// } satisfies Meta<typeof Header>;
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+// export default meta;
+// type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+// export const Primary: Story = {};
+export default {};
