63bde7b4b750083abd6038903fc0a080ebd03fd1
HoshimuraYuto <sukiburo@gmail.com>
2023-11-18 12:56:48 +0000

    add code copy button

---
+++ app/components/elements/CustomCode.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useRef, useState } from "react";
+
+import { Button } from "./Button";
+
+const CustomCode = ({
+  children,
+  ...props
+}: {
+  children: string;
+}): JSX.Element => {
+  const [copied, setCopied] = useState(false);
+  const preRef = useRef<HTMLPreElement>(null);
+
+  const handleCopy = async () => {
+    const codeElement = preRef.current?.querySelector(
+      "code",
+    ) as HTMLElement | null;
+
+    await navigator.clipboard.writeText(codeElement?.textContent ?? "");
+
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
+
+  return (
+    <pre
+      ref={preRef}
+      {...props}
+    >
+      <Button
+        onClick={() => void handleCopy()}
+        size="icon"
+        aria-label="code copy"
+        className="absolute right-[10px] top-[10px]"
+      >
+        {copied ? (
+          <div className="i-carbon-checkmark color-green wh-4" />
+        ) : (
+          <div className="i-carbon-copy wh-4" />
+        )}
+      </Button>
+      {children}
+    </pre>
+  );
+};
+
+export default CustomCode;
